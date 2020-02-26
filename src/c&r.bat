d59411a43ee5397ad6679997ee99b412d0e114f6
Ethan Byeon <ethanbyeon@gmail.com>
2020-02-25 21:24:08 -0800

    Add files via upload

---
+++ src/c&r.bat
@@ -0,0 +1,17 @@
+@echo off
+title NEATO BDAY
+color 02
+
+type cake.txt
+type banner.txt
+
+echo(
+date /t
+time /t
+echo(
+
+javac -Xlint *.java
+pause
+echo Loading NEATO BDAY...
+java -cp .; starter
+pause
